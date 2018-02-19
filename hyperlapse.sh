706199f6c3d7007076baab65d3cb5ac5b5fab3de
Robbie Ferguson <contact@category5.tv>
2018-02-19 09:58:32 -0500

    Add files via upload

---
+++ hyperlapse.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+clear
+echo ""
+echo 'Recording hyperlapse...'
+echo 'When finished, press CTRL-C to save video'
+echo 'But make sure you ONLY PRESS IT ONCE'
+
+folder='/home/tali/hyperlapse'
+cd $folder
+
+newlapse -ccc -rate 1 -fps 24 -folder "$folder" > /dev/null 2>&1
+
+tar -czvf $folder/backup-`date +"%Y-%m-%d-%T"`.tar.gz $folder/*.jpg $folder/1s
+rm $folder/*.jpg
+rm -rf $folder/1s
+
+name=hyperlapse-`date +%Y-%m-%d-%T`
+if [[ -e $name.mp4 ]] ; then
+    i=0
+    while [[ -e $name-$i.mp4 ]] ; do
+        let i++
+    done
+    name=$name-$i
+fi
+mv video_1.mp4 "$name".mp4
+
+echo ""
+echo DONE
+echo File is saved in folder "hyperlapse"
+sleep 5
